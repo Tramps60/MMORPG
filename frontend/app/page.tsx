71a42e4f67c4ab5cedab8febf61a333885333343
Liam Phillips <liamphilips@avensure.com>
2024-12-08 17:12:59 +0000

    context menu

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,14 +4,15 @@ import Game from "./components/Game";
 import { useEffect } from "react";
 import { WEBSOCKET_URL } from "@/constants/game";
 import { useGameStore } from "@/store/game-store";
+import ContextMenu from "./components/UI/ContextMenu";
 
 export default function Home() {
   const { connect, disconnect, isConnected } = useWebsocketStore();
-  const { setPlayer, updateRemotePlayer, deleteRemotePlayer } = useGameStore()
+  const { setPlayer, updateRemotePlayer, deleteRemotePlayer } = useGameStore();
 
   useEffect(() => {
     connect(WEBSOCKET_URL, setPlayer, updateRemotePlayer, deleteRemotePlayer);
-
+    
     return () => {
       disconnect();
     };
@@ -20,7 +21,22 @@ export default function Home() {
   return (
     <div className="h-screen w-full">
       {!isConnected && <div>connecting to game server...</div>}
-      {isConnected && <Game />}
+      {isConnected && (
+        <>
+          <Game />
+          <HUD />
+        </>
+      )}
+    </div>
+  );
+}
+
+export function HUD() {
+  return (
+    <div className="absolute top-0 left-0 w-full h-full pointer-events-none">
+      <div className="pointer-events-auto">
+        <ContextMenu />
+      </div>
     </div>
   );
 }
